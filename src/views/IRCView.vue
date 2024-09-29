d31996de5d8111709bd0107baf0e6574a2529084
Evan Mallery <emallery@vt.edu>
2024-09-29 12:11:42 -0400

    Connect to Twitch IRC anonymously

---
+++ src/views/IRCView.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>Not found</header>
+</template>
+
+<script setup lang="ts">
+
+// let client = new IRC.Client();
+
+const EVENTSUB_WEBSOCKET_URL = 'wss://irc-ws.chat.twitch.tv:443';
+
+let websocketClient = new WebSocket(EVENTSUB_WEBSOCKET_URL);
+
+websocketClient.addEventListener('error', console.error);
+
+websocketClient.addEventListener('open', () => {
+    console.log('WebSocket connection opened to ' + EVENTSUB_WEBSOCKET_URL);
+
+    websocketClient.send("PASS gamer");
+    websocketClient.send("NICK justinfan1234");
+    websocketClient.send("CAP REQ twitch.tv/tags")
+    websocketClient.send("JOIN #gamesdonequick");
+    
+});
+
+websocketClient.addEventListener('message', (data) => {
+    console.log(data.data);
+});
+
+</script>
