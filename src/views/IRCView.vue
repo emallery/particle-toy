13326d9aa036b2c63aae0020778f3e44720c616c
Evan Mallery <emallery@vt.edu>
2024-12-20 01:42:53 -0500

    Finish implementing emote fetching

--- src/views/IRCView.vue
+++ src/views/IRCView.vue
@@ -1,14 +1,36 @@
 <template>
-    <header>Not found</header>
+    <header>Emotes for: <input type="text" v-model="channel"/></header>
+    <body>
+        <ul>
+            <li v-for="emote in emoteList" :key="emote">
+                <img :src="emote"/>
+                 {{ emote }}
+            </li>
+        </ul>
+    </body>
 </template>
 
 <script setup lang="ts">
+import { ref, watch } from 'vue';
 
-// let client = new IRC.Client();
-
+const emoteList = ref([] as Array<string>);
+const channel = ref("gamesdonequick");
 const EVENTSUB_WEBSOCKET_URL = 'wss://irc-ws.chat.twitch.tv:443';
 
 let websocketClient = new WebSocket(EVENTSUB_WEBSOCKET_URL);
+let channelChangeTimer: number | undefined = undefined;
+
+watch(channel, (newChannel, oldChannel) => {
+
+    clearTimeout(channelChangeTimer);
+    channelChangeTimer = setTimeout(() => {
+        console.log("Changed")
+        emoteList.value.length = 0;
+        websocketClient.send(`PART #${oldChannel}`);
+        websocketClient.send(`JOIN #${newChannel}`);
+    }, 1000);
+    
+});
 
 websocketClient.addEventListener('error', console.error);
 
@@ -18,12 +40,27 @@ websocketClient.addEventListener('open', () => {
     websocketClient.send("PASS gamer");
     websocketClient.send("NICK justinfan1234");
     websocketClient.send("CAP REQ twitch.tv/tags")
-    websocketClient.send("JOIN #gamesdonequick");
-    
+    websocketClient.send(`JOIN #${channel.value}`);
 });
 
 websocketClient.addEventListener('message', (data) => {
     console.log(data.data);
+
+    let msg = data.data as string;
+
+    // Get messages
+    if (msg.includes("PRIVMSG")) {
+
+        // Parse emotes from chat message
+        for (let entry of msg.matchAll(/emotes=(.*?);/g)) {
+            if (entry[1]) {                
+                let emoteId = entry[1].split(":")[0];
+                let emoteUrl = `https://static-cdn.jtvnw.net/emoticons/v2/${emoteId}/static/light/3.0`;
+                emoteList.value.push(emoteUrl);
+            }
+            
+        }
+    }
 });
 
 </script>
