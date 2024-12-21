7b8ce80fb45101e65dfc7888d8f52034215f28c7
Evan Mallery <emallery@vt.edu>
2024-12-21 01:06:20 -0500

    Fix incorrect channel being used for PART when changing channel

--- src/views/IRCView.vue
+++ src/views/IRCView.vue
@@ -18,27 +18,30 @@ const channel = ref("gamesdonequick");
 const EVENTSUB_WEBSOCKET_URL = 'wss://irc-ws.chat.twitch.tv:443';
 
 let websocketClient = new WebSocket(EVENTSUB_WEBSOCKET_URL);
-let channelChangeTimer: number | undefined = undefined;
+/** Object to keep track of state related to changing the channel */ 
+const channelChanger: {timerId: number | undefined, prevChannel: string} = {timerId: undefined, prevChannel: channel.value};
 
-watch(channel, (newChannel, oldChannel) => {
-
-    clearTimeout(channelChangeTimer);
-    channelChangeTimer = setTimeout(() => {
-        console.log(`Disconnecting from [${oldChannel}, connecting to [${newChannel}]...`);
-        emoteList.value.length = 0;
-        websocketClient.send(`PART #${oldChannel}`);
+watch(channel, (newChannel) => {
+    // Prevent spamming join/leave requests by only changing channel if text isn't updated for a long enough duration.
+    clearTimeout(channelChanger.timerId);
+    channelChanger.timerId = setTimeout(() => {
+        console.log(`Disconnecting from [${channelChanger.prevChannel}, connecting to [${newChannel}]...`);
+        websocketClient.send(`PART #${channelChanger.prevChannel}`);
         websocketClient.send(`JOIN #${newChannel}`);
+        channelChanger.prevChannel = newChannel;
+        emoteList.value.length = 0;
     }, 1000);
-    
 });
 
 websocketClient.addEventListener('error', console.error);
 
 websocketClient.addEventListener('open', () => {
     console.log('WebSocket connection opened to ' + EVENTSUB_WEBSOCKET_URL);
+    const loginId = Math.floor(Math.random() * 9999999999);
 
+    // Use "justinfan" account with randomly-generated ID and any password to log in anonymously
     websocketClient.send("PASS gamer");
-    websocketClient.send("NICK justinfan1234");
+    websocketClient.send(`NICK justinfan${loginId}`);
     websocketClient.send("CAP REQ twitch.tv/tags")
     websocketClient.send(`JOIN #${channel.value}`);
 });
@@ -54,15 +57,13 @@ websocketClient.addEventListener('message', (data) => {
 
     // Handle chat messages
     if (msg.includes("PRIVMSG")) {
-
         // Parse emotes from chat message
         for (let entry of msg.matchAll(/emotes=(.*?);/g)) {
             if (entry[1]) {                
                 let emoteId = entry[1].split(":")[0];
-                let emoteUrl = `https://static-cdn.jtvnw.net/emoticons/v2/${emoteId}/default/light/3.0`;
+                let emoteUrl = `https://static-cdn.jtvnw.net/emoticons/v2/${emoteId}/default/light/2.0`;
                 emoteList.value.push(emoteUrl);
             }
-            
         }
     }
 
